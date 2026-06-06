51bb988b1fb988263ee273225adb31be3063e605
Monbuticloud <hoangquocanhmon@icloud.com>
2026-06-06 15:51:44 +0700

    feat: add scrolling rainbow animation to name-section

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 let name_section = document.getElementById("name-section");
 let name_section_width = name_section.offsetWidth;
+let number_of_rainbow_segments = 30;
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < number_of_rainbow_segments; i++) {
   let div = document.createElement("div");
-  div.style.backgroundColor = get_rainbow(i / 10);
-  div.style.width = name_section_width / 10 + "px";
+  div.style.backgroundColor = get_rainbow(i / number_of_rainbow_segments);
+  div.style.width = name_section_width / number_of_rainbow_segments + "px";
   name_section.appendChild(div);
 }
 
+function shiftBg(container) {
+  const divs = [...container.querySelectorAll("div")];
+
+  const first = getComputedStyle(divs[0]).backgroundColor;
+
+  for (let i = 0; i < divs.length - 1; i++) {
+    divs[i].style.backgroundColor = getComputedStyle(
+      divs[i + 1],
+    ).backgroundColor;
+  }
+
+  divs[divs.length - 1].style.backgroundColor = first;
+}
+setInterval(shiftBg, 75, name_section);
+
 function get_rainbow(t) {
   t = Math.min(1, Math.max(0, t));
-  const hue = t * 300; // avoids wrapping back to red
-  return `hsl(${hue}, 85%, 55%)`;
+  const hue = t * 359;
+  return `hsl(${hue}, 55%, 65%)`;
 }

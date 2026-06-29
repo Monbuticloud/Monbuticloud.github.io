15a5d35b6b25fa97027a2e70911cddfdff67843b
Monbuticloud <hoangquocanhmon@icloud.com>
2026-06-29 15:56:07 +0700

    feat: link remaining paths

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -17,7 +17,17 @@ fn main() {
         let port = env::var("PORT").unwrap_or_else(|_| "8000".to_string());
         let addr: SocketAddr = format!("0.0.0.0:{port}").parse().expect("Invalid socket address");
 
-        let app = Router::new().route("/", get(get_index_html));
+        let app = Router::new()
+            // index
+            .route("/", get(get_index_html))
+            .route("/index", get(get_index_html))
+            .route("/index.html", get(get_index_html))
+            // contact
+            .route("/contact", get(get_contact_html))
+            .route("/contact.html", get(get_contact_html))
+            // projects
+            .route("/projects", get(get_projects_html))
+            .route("/projects.html", get(get_projects_html));
 
         println!("listening on {addr}");
         stdout().flush().ok();
@@ -30,6 +40,24 @@ fn main() {
 
 async fn get_index_html() -> (StatusCode, Html<String>) {
     match tokio::fs::read_to_string("static/index.html").await {
+        Ok(contents) => (StatusCode::OK, Html(contents)),
+        Err(_) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Html("<h1>Internal Server Error</h1>".to_string()),
+        ),
+    }
+}
+async fn get_contact_html() -> (StatusCode, Html<String>) {
+    match tokio::fs::read_to_string("static/contact.html").await {
+        Ok(contents) => (StatusCode::OK, Html(contents)),
+        Err(_) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Html("<h1>Internal Server Error</h1>".to_string()),
+        ),
+    }
+}
+async fn get_projects_html() -> (StatusCode, Html<String>) {
+    match tokio::fs::read_to_string("static/projects.html").await {
         Ok(contents) => (StatusCode::OK, Html(contents)),
         Err(_) => (
             StatusCode::INTERNAL_SERVER_ERROR,
